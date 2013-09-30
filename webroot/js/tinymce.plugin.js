3334758101ee5fab921f239e83a4a8389a17b4e1
liamkeily <liam@simplywebsites.com>
2013-09-30 18:39:55 +0100

    Missing webroot files

---
+++ webroot/js/tinymce.plugin.js
@@ -0,0 +1,23 @@
+tinymce.PluginManager.add("elfinder", function (editor, url) {
+  editor.settings.file_browser_callback = function (id, value, type, win) {
+    $('<div />').dialogelfinder({
+       url: '/admin/el_finder/el_finder/connector',
+       commandsOptions: {
+          getfile: {
+             oncomplete: 'destroy'
+          }
+       },
+       getFileCallback: function (url) {
+          var fieldElm = win.document.getElementById(id);
+          fieldElm.value = editor.convertURL(url, null, true);
+          if ("fireEvent"in fieldElm) {
+             fieldElm.fireEvent("onchange")
+          } else {
+             var evt = document.createEvent("HTMLEvents");
+             evt.initEvent("change", false, true);
+             fieldElm.dispatchEvent(evt)
+          }
+       }
+    });   
+  }; 
+}, ["../../../el_finder/js"]);
