192d45694b7ecfceed5dd651a26621dba1bebb65
Liam Keily <liam@simplywebsites.com>
2014-03-13 00:59:52 +0000

    Clean up the ElFinder plugin

--- README.md
+++ README.md
@@ -8,3 +8,30 @@ Tested with 2.0 but should work with 1.5+
 
 Clone repository to your plugin directory (Plugin/ElFinder), and activate the plugin from your admin panel.
 
+## Usage
+
+From Media > Files you will be able to manage your files using ElFinder
+
+## External Plugin Usage
+
+Other plugins can use the image picker built into this plugin to select files.
+
+Step 1. Include the script
+
+> $this->Html->script('ElFinder.image_picker',array('inline'=>false));
+
+Step 2. Add an event handler to a button.
+
+`
+$(function(){
+$("#select_image").click(function(){
+imagepicker.getFiles(function(files){
+	console.log(files);
+	for(i in files){
+		var file = files[i];
+	}
+});
+});
+});
+`
+
--- webroot/js/image_picker.js
+++ webroot/js/image_picker.js
@@ -1,46 +1,33 @@
-// Liam's ElFinder File Picker
 var imagepicker = new Object();
-
-$(function(){
-// Create instance of elfinder
-imagepicker.div = $("#images_finder");
-imagepicker.elf = imagepicker.div.elfinder({
-url : '<?php echo $this->Html->url(array('plugin'=>'el_finder','controller'=>'el_finder','action'=>'connector')) ?>',
-height:300
-}).elfinder('instance');
-
-// Create a dialog for elfinder
-imagepicker.dialog = imagepicker.div.dialog({
-autoOpen:false,
-width:750,
-buttons:{
-  "Insert":function(){
-    // get array of files
-    var files = imagepicker.div.find(".ui-selected").map( function() {
-      return imagepicker.elf.path( $(this).attr('id') );
-    }).get();
-    imagepicker.callback_function(files);
-    imagepicker.dialog.dialog('close');
-  }
-}
-});
-
-
-// Get Files Method
 imagepicker.getFiles = function(callback_function){
-imagepicker.callback_function = callback_function;
-imagepicker.dialog.dialog("open");
-}
-});
+	if(!imagepicker.div){
+		imagepicker.div = $("<div></div>");
+	}
 
-/* Example use of callback
-$(function(){
-// Bind select file button
-$("#Images").on('click','.Image_select_file',function(){
-imagepicker.row = $(this).parent();
-imagepicker.getFiles(function(files){
-  imagepicker.row.find('.url').val(files[0]);
-  imagepicker.row.find('.image_preview').attr('src','<?php echo $this->Html->url('/') ?>' + files[0]);
-});
-});
-*/
+	if(!imagepicker.dialog){
+		imagepicker.dialog = imagepicker.div.dialog({
+		autoOpen:false,
+		width:750,
+		buttons:{
+		  "Insert":function(){
+		    // get array of files
+		    var files = imagepicker.div.find(".ui-selected").map( function() {
+		      return imagepicker.elf.path( $(this).attr('id') );
+		    }).get();
+		    imagepicker.callback_function(files);
+		    imagepicker.dialog.dialog('close');
+		  }
+		}
+		});
+	}
+
+	if(!imagepicker.elf){
+		imagepicker.elf = imagepicker.div.elfinder({
+		url : '<?php echo ClassRegistry::init('HtmlHelper')->url(array('plugin'=>'el_finder','controller'=>'el_finder','action'=>'connector','admin'=>true)) ?>',
+		height:300
+		}).elfinder('instance');
+	}
+
+	imagepicker.callback_function = callback_function;
+	imagepicker.dialog.dialog("open");
+}
